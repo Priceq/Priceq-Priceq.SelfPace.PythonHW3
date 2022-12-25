ead3b8afcf7723f047069d978be228a189cb12e5
Niko Price <priceq.05@gmail.com>
2022-12-25 01:11:08 -0600

    first commit

---
+++ hwmod.py
@@ -0,0 +1,15 @@
+def footage(length, width):
+    sqft = length * width
+    return sqft
+
+def Circumference():
+    radius = input("what is the diameter?")
+    circumference= (radius * 2 * 3.14)
+    return circumference   
+
+def printName(name):
+    print(f"Hello Mr/Ms {name}... we've been waiting for you!")
+    
+def print_your_name_please(name):
+    print(f"Hello Mr/Ms {name}... we've been waiting for you!")
+    
---
+++ module.py
@@ -0,0 +1,16 @@
+def printName(name):
+    return f"Hello Mr/Ms {name}...we've been waiting for you!"
+    
+def footage(length, width):
+    sqft = length * width
+    return sqft
+
+def Circumference():
+    radius = int(input("what is the diameter?"))
+    circumference= (radius * 2 * 3.14)
+    return circumference   
+
+def print_your_name_please(name):
+    print(f"Hello Mr/Ms {name}... we've been waiting for you!")
+    
+    
